5bd6285a34068ee4b792f90af3b2daa05722a4f0
Esdras López <yahir@tdisa.net>
2020-10-14 00:39:16 -0500

    Cambios esteticos y se agrego nullable al idTipoGallo

---
+++ Gallería/Migrations/20201014002148_addNullableIdTipoGallo.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gallería.Migrations
+{
+    public partial class addNullableIdTipoGallo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gallo_TipoGallo_Id_TipoGallo",
+                table: "Gallo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id_TipoGallo",
+                table: "Gallo",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gallo_TipoGallo_Id_TipoGallo",
+                table: "Gallo",
+                column: "Id_TipoGallo",
+                principalTable: "TipoGallo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gallo_TipoGallo_Id_TipoGallo",
+                table: "Gallo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id_TipoGallo",
+                table: "Gallo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gallo_TipoGallo_Id_TipoGallo",
+                table: "Gallo",
+                column: "Id_TipoGallo",
+                principalTable: "TipoGallo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Gallería/Models/Gallo.cs
+++ Gallería/Models/Gallo.cs
@@ -24,7 +24,7 @@ namespace Gallería.Models
         public DateTime FechaNacimiento { get; set; } = DateTime.Now;
 
         [ForeignKey("TipoGallo")]
-        public int Id_TipoGallo { get; set; }
+        public int? Id_TipoGallo { get; set; }
         public TipoGallo TipoGallo { get; set; }
     }
 }
